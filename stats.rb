e2e07cb55f535f6816b712a5874026868be17aff
Siddharth <svs@intellecap.net>
2010-07-17 22:04:47 +0530

    log file

--- stats.rb
+++ stats.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/ruby
+LOG_FILE = '/opt/nginx/logs/access.log'
 f = File.open('views/posts/stats','w')
 f.write "<br><br>\n\n"
-hits = `cat access.log |  awk '{print $7}' | grep haml | wc -l`
-visitors = `awk '{print $1, $7'} < access.log | grep haml | awk '{print $1}' | sort | uniq | wc -l`
+hits = `cat #{LOG_FILE} |  awk '{print $7}' | grep haml | wc -l`
+visitors = `awk '{print $1, $7'} < #{LOG_FILE} | grep haml | awk '{print $1}' | sort | uniq | wc -l`
 f.write("p(hits). *#{hits.rstrip}* hits\n*#{visitors.rstrip}* unique visitors\n\n")
 f.write("h2. Most Popular\n\n")
 f.write("|_. Title |_. Hits|\n")
-f.write(`cat access.log |  awk '{print $7}' | grep haml | sed 's/?\S*//' | sort | uniq -c | sort -r -n | head -n 10 | awk '{print "| ", $2," | ",$1," |"}' | sed -e 's/\\/blog\\///' -e 's/\+/ /g' -e 's/\.haml//'`)
+f.write(`cat #{LOG_FILE} |  awk '{print $7}' | grep haml | sed 's/?\S*//' | sort | uniq -c | sort -r -n | head -n 10 | awk '{print "| ", $2," | ",$1," |"}' | sed -e 's/\\/blog\\///' -e 's/\+/ /g' -e 's/\.haml//'`)
 f.write("\nh2. latest visitors\n\n")
-f.write(`tail -n 100 access.log | grep haml | awk '{print "|",$1,"|", $7,"|"}'`)
+f.write(`tail -n 100 #{LOG_FILE} | grep haml | awk '{print "|",$1,"|", $7,"|"}'`)
 f.close
 
 
