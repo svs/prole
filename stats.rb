17ae2501eeb61581d41333fc50508bd8e87889fb
Siddharth <svs@intellecap.net>
2010-07-20 01:05:44 +0530

    |

--- stats.rb
+++ stats.rb
@@ -13,7 +13,7 @@ f.write(`cat #{LOG_FILE} |  awk '{print $7}' | grep haml | sed 's/?\S*//' | sort
 f.write("\nh2. latest visitors\n\n")
 lv = `tail -n 100 #{LOG_FILE} | awk '{if ($7 ~ /haml/) print $1, $7, $11'}`.split("\n").map{|x| x.split(" ")}
 lv = lv.map{|v| [`host #{v[0]}`, v[1], v[2]]}
-lv.each {|x| f.write("|#{x[0].split(" ")[4]}|#{x[1]}|#{x[2]}\n")}
+lv.each {|x| f.write("|#{x[0].split(" ")[4]}|#{x[1]}|#{x[2]}|\n")}
 f.write "\n\nh2. Hits by Month\n\n"
 f.write `cat #{LOG_FILE} | awk '{print $4, $7}' | grep haml | awk '{print $1}' | sed -e 's/:[0-9][0-9]//g' -e 's/\\[//' -e 's/[0-9][0-9]\\///' | uniq -c | awk '{print "|",$2,"|",$1,"|"}'`
 f.write "\n\nh2. Top Referers\n\n"
