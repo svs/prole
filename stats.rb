8efe5706717fa9e99e1c397ff6a319357a122a5a
Siddharth <svs@intellecap.net>
2010-07-20 01:38:12 +0530

    runaway regex

--- stats.rb
+++ stats.rb
@@ -15,7 +15,7 @@ lv = `tac #{LOG_FILE} | awk '{if ($7 ~ /haml/) print $1, $7, $11}' | head -n 15`
 lv = lv.map{|v| [`host #{v[0]}`, v[1], v[2]]}
 lv.each {|x| f.write("|#{x[0].split(" ")[4]}|#{x[1]}|#{x[2]}|\n")}
 f.write "\n\nh2. Hits by Month\n\n"
-f.write `cat #{LOG_FILE} | awk '{if ($7 ~ /haml) print $4}' | sed -e 's/:[0-9][0-9]//g' -e 's/\\[//' -e 's/[0-9][0-9]\\///' | uniq -c | awk '{print "|",$2,"|",$1,"|"}'`
+f.write `cat #{LOG_FILE} | awk '{if ($7 ~ /haml/) print $4}' | sed -e 's/:[0-9][0-9]//g' -e 's/\\[//' -e 's/[0-9][0-9]\\///' | uniq -c | awk '{print "|",$2,"|",$1,"|"}'`
 f.write "\n\nh2. Top Referers\n\n"
 f.write `cat #{LOG_FILE} | awk '{if($7 ~ /haml/) print $7, $11}' | more | grep -v "_-_" | awk '{print $2'} | sort | uniq -c | sort -r -n | grep -v "\"-\"" | head -10 | awk '{print "|", $2, "|", $1, "|"}'`
 
